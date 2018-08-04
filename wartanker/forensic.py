234aa84d0bdd01e8306e05ae8bf899d660264ff9
uhmtoto <uhmtotow@gmail.com>
2018-08-04 11:38:14 +0900

    forensic 오류 수정

--- wartanker/forensic.py
+++ wartanker/forensic.py
@@ -2,7 +2,7 @@ import os
 import re
 
 def get_fileinfo(file_path):
-    return os.system('file ' + path)    
+    return os.system('file ' + file_path)    
 
 def find_flag(regex, file_path):
     file = open(file_path, 'r')
