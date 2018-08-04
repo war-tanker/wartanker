ebdc6e305167e7b3d7d40614ef90716cc1c6fb1a
uhmtoto <uhmtotow@gmail.com>
2018-08-04 12:07:26 +0900

    update hash

--- wartanker/hash.py
+++ wartanker/hash.py
@@ -1,34 +1,32 @@
 import hashlib
 
-md5 = hashlib.md5()
-sha = hashlib.sha()
-sha1 = hashlib.sha1()
-sha224 = hashlib.sha224()
-sha256 = hashlib.sha256()
-sha384 = hashlib.sha384()
-sha512 = hashlib.sha512()
-
-def get_hashtype(string):
-    
-
 def md5encode(string):
-    return md5.update(string).hexdigest()
-
-def shaencode(string):
-    return sha.update(string).hexdigest()
+    md5 = hashlib.md5()
+    md5.update(string)
+    return md5.hexdigest()
 
 def sha1encode(string):
-    return sha1.update(string).hexdigest()
+    sha1 = hashlib.sha1()
+    sha1.update(string)
+    return sha1.hexdigest()
 
 def sha256encode(string):
-    return sha256.update(string).hexdigest()
+    sha256 = hashlib.sha256()
+    sha256.update(string)
+    return sha256.hexdigest()
 
 def sha384encode(string):
-    return sha384.update(string).hexdigest()
+    sha384 = hashlib.sha384()
+    sha384.update(string)
+    return sha384.hexdigest()
 
 def sha512encode(string):
-    return sha512.update(string).hexdigest()
+    sha512 = hashlib.sha512()
+    sha512.update(string)
+    return sha512.hexdigest()
 
 def sha_encode(sha, string):
-    return eval('hashlib.sha' + str(sha) + '().update(' + string + ').hexdigest()')
+    exec('sha = hashlib.sha' + str(sha) + '()')
+    exec('sha.update(\'' + string + '\')')
+    return sha.hexdigest()
     
