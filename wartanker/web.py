c07b3d76a3b041820bdc770e418bb4b823ecbf15
엄서훈 (Uhm Seohun) <uhmtotow@gmail.com>
2018-08-05 17:04:06 +0900

    Python3 문법에 맞도록 수정

--- wartanker/web.py
+++ wartanker/web.py
@@ -18,7 +18,7 @@ def xss_exploit(method, URL, name, param, cookie, testURL):
             requests.post(URL, cookies=cookie, data=param)
         
         res = requests.get(testURL, cookies=cookie).text
-        print '[*] trying ' + str(i) + ' of ' + str(data_len)
+        print ('[*] trying ' + str(i) + ' of ' + str(data_len))
         if data[i] in res:
             print ('[*] How about trying \'' + data[i] + '\'?')
             more = raw_input('[*] Do you want more? [Y / N] ')
