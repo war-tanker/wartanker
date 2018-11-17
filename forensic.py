7ad8789c504d6bbf286310c3b500d3698b59c494
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-11-17 10:47:19 +0900

    UPDATE: forensic.py

--- forensic.py
+++ forensic.py
@@ -2,16 +2,16 @@ import sys, subprocess, re
 
 class UnsupportedOsError(Exception): 
     def __init__(self):
-        super().__init__('this method does not support current OS')
+        super().__init__('This method does not support current OS')
 
 def get_fileinfo(file_path):
-    if not sys.platform.startswith('win'):
+    if not sys.platform.startswith('win'): # only on linux/OS X systems
         output = subprocess.Popen(['file', file_path], stdout=subprocess.PIPE).communicate()[0]
         return output.decode().strip()
     else: raise UnsupportedOsError
 
 def find_flag(flag, file_path):
-    file = open(file_path, 'r')
+    file = open(file_path, 'rb')
     data = file.read()
     p = re.compile(flag + '\{.{0,}\}')
     return p.findall(data)
