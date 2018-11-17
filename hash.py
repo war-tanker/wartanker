17fc67ba784bb266e1f2439976f6d4e5e921853d
Uhm Seohun <uhmtotow@gmail.com>
2018-11-17 13:36:29 +0900

    FIX: hash.py

--- hash.py
+++ hash.py
@@ -2,30 +2,38 @@ import hashlib
 
 def md5encode(string):
     md5 = hashlib.md5()
-    md5.update(string)
+    md5.update(string.encode('utf-8'))
     return md5.hexdigest()
 
 def sha1encode(string):
     sha1 = hashlib.sha1()
-    sha1.update(string)
+    sha1.update(string.encode('utf-8'))
     return sha1.hexdigest()
 
 def sha256encode(string):
     sha256 = hashlib.sha256()
-    sha256.update(string)
+    sha256.update(string.encode('utf-8'))
     return sha256.hexdigest()
 
 def sha384encode(string):
     sha384 = hashlib.sha384()
-    sha384.update(string)
+    sha384.update(string.encode('utf-8'))
     return sha384.hexdigest()
 
 def sha512encode(string):
     sha512 = hashlib.sha512()
-    sha512.update(string)
+    sha512.update(string.encode('utf-8'))
     return sha512.hexdigest()
 
 def sha_encode(sha, string):
-    exec('sha = hashlib.sha' + str(sha) + '()')
-    exec('sha.update(\'' + string + '\')')
-    return sha.hexdigest()
+    sha = int(sha)
+    if sha==1:
+        sha_=hashlib.sha1()
+    elif sha==256:
+        sha_=hashlib.sha256()
+    elif sha==384:
+        sha_=hashlib.sha384()
+    elif sha==512:
+        sha_=hashlib.sha512()
+    sha_.update(string.encode('utf-8'))
+    return sha_.hexdigest()
